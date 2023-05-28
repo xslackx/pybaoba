fe5509fcd19cfe3bc0b2b8f60cdeb777a149b586
Ruan <xslackx@live.fr>
2023-05-28 19:29:45 -0300

    Append new error msg on st_messages and update osLike

--- baoba/utils/oslike.py
+++ baoba/utils/oslike.py
@@ -1,12 +1,27 @@
-import os, usermsg
+import os, usermsg, re
 
 def osLike():
     os_release="/etc/os-release"
+    os_support="../conf/supported.json"
+    supported = usermsg.loadFile(os_support)
+
     if os.access(os_release, os.R_OK):
         with open(os_release, "r") as f:
             file = f.read()
+            file = file.split("\n")
             f.close()
-        return file.split("\n")
+
+        for key in file:
+            if re.match(r"ID=(.*)", key):
+                grep = re.match(r"ID=(.*)", key)
+        if not grep:
+            return usermsg.userMsg('err', 'f04')
+
+        for id in supported["systems"]["ID"]:
+            if id == grep[1]:
+                return id
+
+        if not id:
+                return usermsg.userMsg('err', 'f03')
     else:
-        msg = usermsg.userMsg('err', 'f01')
-        return msg
+        return usermsg.userMsg('err', 'f01')
