151d8342f4d2d85857b40df5a7d2ed026aa36fc4
Ruan <xslackx@live.fr>
2023-05-28 14:49:48 -0300

    Update usermsg, rename usermsg.json to st_messages.json

--- baoba/utils/usermsg.py
+++ baoba/utils/usermsg.py
@@ -1,8 +1,16 @@
-import sys
-sys.append('../conf/')
+import json
+
+def loadFile():
+    status = "../conf/st_messages.json"
+        with open(status, "r+") as file:
+            msg = json.load(file)
+            file.close()
+        return msg
 
 def userMsg(type, st_code):
-    if type != 'info' and
-    type != 'ok' and
-    type != 'err':
-        pass
+    msg = loadFile()
+    if type == None or st_code == None:
+        return msg["status"]["err"]["f02"]
+
+    if type != 'info' and type != 'ok' and type != 'err':
+        return  msg["status"]["err"]["f01"]
