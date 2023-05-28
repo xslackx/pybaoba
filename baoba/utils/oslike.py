fa6bd250d69452721e6ecb7cecc5595316739397
Ruan <xslackx@live.fr>
2023-05-28 09:37:10 -0300

    Update osLike, add msg status

--- baoba/utils/oslike.py
+++ baoba/utils/oslike.py
@@ -1,6 +1,14 @@
+import os
 def osLike():
     os_release="/etc/os-release"
-    with open(os_release, "r") as f:
-        file = f.read()
-        f.close()
-    return file.split("\n")
+    if os.access(os_release, os.R_OK):
+        with open(os_release, "r") as f:
+            file = f.read()
+            f.close()
+        return file.split("\n")
+    else:
+        msg = {
+            "status": "fail",
+            "erro": "os-release don't exists"
+        }
+        return msg
