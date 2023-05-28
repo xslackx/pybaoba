1fce6dc302b1315dcf4f1c49ef563ea24b200afa
Ruan <xslackx@live.fr>
2023-05-28 20:26:00 -0300

    Update packmanager to return cmdlet with specific OS

--- baoba/utils/packmanager.py
+++ baoba/utils/packmanager.py
@@ -1,2 +1,17 @@
-def packMan():
-    pass
+import usermsg
+
+def packMan(id):
+    ids = "../conf/supported.json"
+    sys = usermsg.loadFile(ids)
+
+    for os in sys["system"]["ID"]:
+        if os == id and os == "fedora" or os == "redhat":
+            cmdlet = "dnf list installed"
+
+        if os == id and os == "debian" or os == "ubuntu":
+            cmdlet = "apt list --installed"
+
+        if os == id and os == "alpine":
+            cmdlet = "apk list -I"
+
+    return cmdlet
