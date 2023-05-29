92c111cd20d42c2ac6aac2c86e43e1e1ec1328f5
Ruan <xslackx@live.fr>
2023-05-29 09:02:10 -0300

    Add permit CMD on supported.json, new err msg, update packIns

--- baoba/utils/packinstalled.py
+++ baoba/utils/packinstalled.py
@@ -1,4 +1,13 @@
-import subprocess, json
+import subprocess, json, usermsg
 
 def packIns(cmd):
+    msg = "../conf/st_messages.json"
+    cmdOk = "../conf/supported.json"
+
+    msg = usermsg.loadFile(msg)
+    letcmd = usermsg.loadFile(cmdOk)
+
+    for permited in letcmd["systems"]["cmd"]:
+    if cmd:
+
     pass
