082922fc00405386b412672ad2aafb6f3d7565f6
Ruan <xslackx@live.fr>
2023-06-03 02:22:06 -0300

    Update packformat.py
    Create a hitcount and stringify everything

--- baoba/utils/packformat.py
+++ baoba/utils/packformat.py
@@ -1,4 +1,5 @@
 import json
+from collections import Counter
 
 def packFmt(pkgs, id):
     if id == "ubuntu":
@@ -25,5 +26,33 @@ def packFmt(pkgs, id):
         if x != None:
             packStorage.append(eachPack(packs[x]))
 
+    hits = hitCount(json.dumps(packStorage))
     
-    return json.dumps(packStorage)
+    packStorage.append(hits)
+    
+    return packStorage
+
+def hitCount(packStorage):
+    allPacks = json.loads(packStorage)
+    packTotal = str(len(allPacks))
+    hit = {
+        "hits": {
+            "arch": {
+            },
+            "group": {
+
+            },
+            "total": packTotal,
+        }
+    }
+    
+    hit64 = []
+    groups = []
+    for x in range(len(allPacks)):
+        hit64.append(allPacks[x]["arch"])
+        groups.append(allPacks[x]["group"][0])
+
+    hit["hits"]["arch"] = Counter(hit64)
+    hit["hits"]["group"] = Counter(groups)
+
+    return json.dumps(hit)
