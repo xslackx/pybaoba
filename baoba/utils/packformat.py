e3b7fbd7f6864ad6e4d25c0fbc313ecb27a16d3a
Ruan <xslackx@live.fr>
2023-06-03 01:21:11 -0300

    Update packformat.py
    Add ubuntu filter

--- baoba/utils/packformat.py
+++ baoba/utils/packformat.py
@@ -1,4 +1,26 @@
-import json
+def packFmt(pkgs, id):
+    if id == "ubuntu":
+        packs = pkgs.split("\n")
+        
+        for aptwarning in range(4):
+            packs.pop(0)
 
-def packFmt():
-    pass
+    def eachPack(p):
+        packName = p.split()[0].split("/")[0]
+        packGroup = p.split()[0].split("/")[1].split(",")
+        packVersion = p.split()[1]
+        packArch = p.split()[2]
+
+        return {
+            "name": packName,
+            "group": packGroup,
+            "version": packVersion,
+            "arch": packArch
+        }
+    
+    packStorage = []
+    for x in range(len(packs)):
+        if x  != None:
+            packStorage.append(eachPack(packs[x]))
+
+    return packStorage
